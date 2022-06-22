1fec8bfc73afbf995db13a8d0f4a45cab52b0e79
omarmans97 <107950532+omarmans97@users.noreply.github.com>
2022-06-23 00:21:47 +0300

    Add files via upload

---
+++ phoneVerification/phoneVerification/src/main/java/com/example/phoneVerification/PhoneVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.phoneVerification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneVerificationApplication.class, args);
+	}
+
+}
